f47a8049cf9b1501a0010a0b62c67d148ebd50b2
Evgeniy Zuykin <eugenzuy@gmail.com>
2017-10-15 14:10:24 +0700

    add ignite

--- src/main/java/com/shaad/ignite/configuration/RepositoryModule.java
+++ src/main/java/com/shaad/ignite/configuration/RepositoryModule.java
@@ -4,10 +4,23 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.shaad.ignite.repo.CellRepository;
 import com.shaad.ignite.repo.CellRepositoryImpl;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.ConnectorConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
 
 public class RepositoryModule extends AbstractModule {
     @Override
     protected void configure() {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setClientMode(true);
+        ConnectorConfiguration connCfg = new ConnectorConfiguration();
+        connCfg.setHost("127.0.0.1");
+        connCfg.setPort(11211);
+        connCfg.setThreadPoolSize(10);
+        cfg.setConnectorConfiguration(connCfg);
+
+        bind(Ignite.class).toInstance(Ignition.start(cfg));
         bind(CellRepository.class).to(CellRepositoryImpl.class).in(Singleton.class);
     }
 }
--- src/main/java/com/shaad/ignite/repo/CellRepositoryImpl.java
+++ src/main/java/com/shaad/ignite/repo/CellRepositoryImpl.java
@@ -1,12 +1,21 @@
 package com.shaad.ignite.repo;
 
 import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 import com.shaad.ignite.domain.Profile;
+import org.apache.ignite.Ignite;
 
 import java.util.Collection;
 import java.util.Date;
 
 public class CellRepositoryImpl implements CellRepository {
+    private final Ignite ignite;
+
+    @Inject
+    public CellRepositoryImpl(Ignite ignite) {
+        this.ignite = ignite;
+    }
+
     @Override
     public boolean doesCellExist(long cellId) {
         return true;
